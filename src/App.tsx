1f44002891d70c0be4ae1e41a834da7efcb150eb
EloisaOliveira <eloisa.oliveirasantos@gmail.com>
2022-01-14 16:55:14 -0300

    criado Summary Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { GlobalStyle } from "./assets/styles/global";
+import { Dasboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 
 export function App() {
   return (
     <>
       <Header/>
+      <Dasboard />
       <GlobalStyle />
     </>
   );
